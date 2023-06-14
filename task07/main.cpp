f2864edd7b1d5b5718e7b64a2926025792192411
Yiyi Cai <caiyiyi1998@gmail.com>
2023-06-15 01:58:18 +0900

    task07 finished

--- task07/main.cpp
+++ task07/main.cpp
@@ -26,6 +26,8 @@ void solve_laplace_gauss_sidel_on_grid(
       if (vtx2isfix[idx_center]) { continue; }
       // write some code below to implement Gauss-Sidel method
       // Do not write more than 5 lines of code
+      // to solve Laplace equation on a grid
+      vtx2val[idx_center] = 0.25f * (vtx2val[idx_center - 1] + vtx2val[idx_center + 1] + vtx2val[idx_center - grid_size] + vtx2val[idx_center + grid_size]);
     }
   }
 }
